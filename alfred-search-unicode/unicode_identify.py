80e268891f7c7294419547a82d2163fb8d4b88dd
Jeff Byrnes <thejeffbyrnes@gmail.com>
2020-10-07 09:48:32 -0400

    Fix: Ensure scripts are portable
    This avoids the potential case of python3 being at a different path.

--- alfred-search-unicode/unicode_identify.py
+++ alfred-search-unicode/unicode_identify.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python3
+#!/usr/bin/env python3
 
 """
 Identify characters from Unicode 12.1 Descriptions
--- alfred-search-unicode/unicode_search.py
+++ alfred-search-unicode/unicode_search.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python3
+#!/usr/bin/env python3
 
 """
 Search for Unicode 12.1 Descriptions
